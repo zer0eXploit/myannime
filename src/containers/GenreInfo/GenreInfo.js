dc3956e2a861e743852d1021acc5454207abdf50
Yan Waipann <yanwaipann@gmail.com>
2020-11-06 13:03:23 +0630

    Add Genres

---
+++ src/containers/GenreInfo/GenreInfo.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Paper, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSnackbar } from "notistack";
+import { Helmet } from "react-helmet";
+import Loader from "../../components/Loader/Loader";
+import Card from "../../components/Card/Card";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  genreIntro: {
+    padding: "2%",
+    borderRadius: "0",
+    marginBottom: "3%",
+  },
+  title: {
+    marginBottom: "15px",
+  },
+}));
+
+const GenreInfo = (props) => {
+  const classes = useStyles();
+  const [genreInfo, setGenreInfo] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+  const genreName = props.location.pathname.split("/")[2];
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://127.0.0.1:5000/v1/genre/${genreName}`)
+      .then((response) => {
+        setGenreInfo(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        enqueueSnackbar("Something went wrong! :(", {
+          variant: "error",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
+      });
+  }, [genreName, enqueueSnackbar]);
+
+  let toRender = null;
+  let animeCards = null;
+
+  if (isLoading) {
+    toRender = <Loader />;
+  } else if (!isLoading && genreInfo) {
+    if (genreInfo.animes.length) {
+      animeCards = genreInfo.animes.map((anime) => {
+        return (
+          <Grid
+            item
+            container
+            justify="center"
+            key={anime.anime_id}
+            xs={6}
+            sm={4}
+            md={3}
+            lg={2}
+          >
+            <Card {...anime} isZawgyi={false} />
+          </Grid>
+        );
+      });
+    } else {
+      animeCards = (
+        <p className={classes.title}>
+          Currently, there is no anime in this genre. Please check back again.
+        </p>
+      );
+    }
+
+    toRender = (
+      <Fragment>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{genreInfo.genre_name} | MYAN-nime</title>
+        </Helmet>
+        <Grid container direction="column" justify="right">
+          <Paper className={classes.genreIntro}>
+            <Typography variant={"h5"}>{genreInfo.genre_name}</Typography>
+            <Typography variant={"subtitle1"}>
+              {genreInfo.genre_explanation}
+            </Typography>
+          </Paper>
+          <Grid
+            item
+            container
+            xs={12}
+            sm={10}
+            style={{ maxWidth: "1500px", padding: "2%", paddingTop: "0.2%" }}
+          >
+            <Typography variant={"h5"} className={classes.title}>
+              {"Anime List"}
+            </Typography>
+            <Grid container spacing={1} justify="left">
+              {animeCards}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Fragment>
+    );
+  }
+
+  return toRender;
+};
+
+export default GenreInfo;
---
+++ src/containers/Genres/Genres.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import { Helmet } from "react-helmet";
+import Loader from "../../components/Loader/Loader";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(16),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  margin: {
+    margin: "5px",
+  },
+  genres: { display: "flex", justifyContent: "center", width: "100%" },
+  genresContainer: {
+    maxWidth: "1000px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  accordion: { borderRadius: "0 !important" },
+}));
+
+const Genres = (props) => {
+  const classes = useStyles();
+  const [genresInfo, setGenresInfo] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/v1/genres")
+      .then((response) => {
+        setGenresInfo(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  let dynamicContent = <Loader />;
+
+  if (genresInfo["genres"]) {
+    dynamicContent = genresInfo["genres"].map((genre) => {
+      return (
+        <Grid item lg={6} md={12} xs={12} key={genre.genre_name}>
+          <Accordion key={genre.genre_name} className={classes.accordion}>
+            <AccordionSummary
+              className={classes.margin}
+              expandIcon={"⮟"}
+              aria-controls="panel1a-content"
+              id={genre.genre_name}
+            >
+              <Typography className={classes.heading}>
+                {genre.genre_name}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {genre.genre_explanation}
+                <br />
+                <br />
+                <NavLink
+                  style={{
+                    color: "black",
+                  }}
+                  to={{
+                    pathname: "/Genre/" + genre.genre_name.replace(/ /g, "-"),
+                  }}
+                >
+                  [See Animes]
+                </NavLink>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      );
+    });
+  }
+
+  return (
+    <div className={classes.genres}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Genres | MYAN-nime</title>
+      </Helmet>
+      <Grid container className={classes.genresContainer}>
+        {/* <Typography variant="h5">{"Genre Information"}</Typography> */}
+        <Grid container justify="center">
+          {dynamicContent}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Genres;
