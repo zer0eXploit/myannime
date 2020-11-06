9c693c4f1621e2b880022df0c367136c4ce711f5
Yan Waipann <yanwaipann@gmail.com>
2020-11-06 13:03:05 +0630

    Update to use hooks and video info fetching method

--- src/containers/Video/Video.js
+++ src/containers/Video/Video.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import {
   Grid,
@@ -9,7 +9,6 @@ import {
   ListItemText,
   Paper,
   Typography,
-  Link,
   Button,
 } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
@@ -21,251 +20,191 @@ import Loader from "../../components/Loader/Loader";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import EpisodesList from "../../components/EpisodesList/EpisodesList";
 import toZawgyi from "../../util/convertToZg";
-import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 import * as actions from "../../store/actions/index";
 
 import classes from "./Video.module.css";
 
-class Video extends Component {
-  SHOW_EPISODES = "ဇာတ်လမ်း အပိုင်းများအားပြပါ။";
-  ADS_INFO = `
-    ကြော်ငြာတွေအတွက် ခွင့်လွှတ်ပါ။ Server Beta က ကြော်ငြာမရှိပါဘူး။ 
-    ဒါပေမဲ့ တစ်ခါတလေ Error တက်နိုင်ပါတယ်။ ပြီးတော့ Anime မစုံပါဘူး။ ^_^ 
+const Video = (props) => {
+  const SHOW_EPISODES = "ဇာတ်လမ်း အပိုင်းများအားပြပါ။";
+  const THANKS_MESSAGE = `
+    မြန်နီမေးကို အသုံးပြုပေးလို့ ကျေးဇူးတင်ပါတယ်။  ^_^ 
     `;
-  ERROR_REPORT = "Error ဖြစ်လျှင် ဆက်သွယ်ရန် ";
-  LIKE_SOCIAL =
-    "နောက်ဆုံး Update တွေကိုသိဖို့ Facebook Page ကို Like လုပ်ပေးထားကြပါဦး။";
-  SHOW_COMMENTS = "Comments များကိုပြပါ။";
-  state = {
-    showComments: false,
+  const SHOW_COMMENTS = "Comments များကိုပြပါ။";
+  const urlParams = props.location.pathname.split("/");
+  const episodeId = urlParams[3];
+  const redirectFunc = props.history.push;
+  const disqusShortname = "myannime";
+  const disqusConfig = {
+    url: `https://myannime.com${props.location.pathname}`,
+    identifier: `myannime.com${props.location.pathname}`,
+    title: `${urlParams[2]} ${urlParams[3]}`,
+  };
+  const disqusTag = (
+    <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+  );
+  let dynamicVideoPlayer = null;
+  let serverSelector = null;
+  let dynamicContent = null;
+  let episodesList = (
+    <ListItem button component={RouterLink} to={"/Anime/" + urlParams[2]}>
+      <ListItemText
+        primary={props.isZawgyi ? toZawgyi(SHOW_EPISODES) : SHOW_EPISODES}
+      />
+    </ListItem>
+  );
+
+  const { episodeInfo, handleFetchVideoData } = props;
+  const [showComments, setShowComments] = useState(false);
+  const [streamUrls, setStreamUrls] = useState([]);
+  const [currentUrl, setCurrentUrl] = useState("");
+
+  useEffect(() => {
+    handleFetchVideoData(episodeId, redirectFunc);
+    setShowComments(false);
+  }, [handleFetchVideoData, episodeId, redirectFunc]);
+
+  useEffect(() => {
+    if (episodeInfo) {
+      const urls = [];
+      for (let i = 0; i <= 5; i++) {
+        if (episodeInfo["episode_uri_" + i]) {
+          urls.push(episodeInfo["episode_uri_" + i]);
+        }
+      }
+      setStreamUrls(urls);
+      setCurrentUrl(urls[0]);
+    }
+  }, [episodeInfo]);
+
+  const handleChangeStreamUrl = (newUrl) => {
+    setCurrentUrl(newUrl);
   };
 
-  updateVideo() {
-    if (this.props.loading) {
-      return;
-    }
-    if (this.props.match.params.episode) {
-      if (
-        !this.props.currentEpisode ||
-        this.props.currentEpisode !== this.props.match.params.episode
-      ) {
-        this.setState({
-          showComments: false,
-        });
-        const urlParams = this.props.location.pathname.split("/");
-        const pathName = urlParams[2] + "/" + urlParams[3];
-        const redirectFunc = this.props.history.push;
-        this.props.handleFetchVideoData(pathName, redirectFunc);
-      }
-    }
-  }
-
-  componentDidMount() {
-    this.updateVideo();
-  }
-
-  componentDidUpdate() {
-    this.updateVideo();
-  }
-
-  render() {
-    const pathNames = this.props.location.pathname.split("/");
-    const disqusShortname = "myannime";
-    const disqusConfig = {
-      url: `https://myannime.com${this.props.location.pathname}`,
-      identifier: `myannime.com${this.props.location.pathname}`,
-      title: `${pathNames[2]} ${pathNames[3]}`,
-    };
-    const disqusTag = (
-      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+  if (streamUrls.length) {
+    serverSelector = (
+      <Select
+        labelId="server-select-label"
+        id="server-select"
+        value={currentUrl}
+        className={classes.ServerSelect}
+        onChange={(event) => {
+          handleChangeStreamUrl(event.target.value);
+        }}
+      >
+        {streamUrls.map((name, idx) => {
+          return (
+            <MenuItem key={idx} value={name}>
+              {"Server " + (idx + 1)}
+            </MenuItem>
+          );
+        })}
+      </Select>
     );
-    let dynamicVideoPlayer = null;
-    let serverSelector = null;
-    let dynamicContent = null;
-    let episodesList = (
-      <ListItem button component={RouterLink} to={"/Anime/" + pathNames[2]}>
-        <ListItemText
-          primary={
-            this.props.isZawgyi
-              ? toZawgyi(this.SHOW_EPISODES)
-              : this.SHOW_EPISODES
-          }
-        />
-      </ListItem>
+  }
+
+  if (currentUrl) {
+    dynamicVideoPlayer = (
+      <iframe
+        title={currentUrl}
+        src={currentUrl}
+        frameBorder="0"
+        marginWidth="0"
+        marginHeight="0"
+        scrolling="no"
+        width="100%"
+        height="100%"
+        allowFullScreen
+      ></iframe>
     );
+  }
 
-    if (this.props.streamUrls && this.props.streamUrls.length) {
-      serverSelector = (
-        <Select
-          labelId="server-select-label"
-          id="server-select"
-          value={this.props.currentVideoServerName}
-          className={classes.ServerSelect}
-          onChange={(event) => {
-            this.props.handleChangeStreamUrl(
-              event.target.value,
-              this.props.serverNameUrlMap[event.target.value]
-            );
-          }}
-        >
-          {this.props.videoServers.map((name, idx) => {
-            return (
-              <MenuItem key={idx} value={name}>
-                {"Server " + name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      );
-    }
-
-    if (this.props.streamUrls && this.props.streamUrls.length) {
-      if (this.props.currentVideoServerName === "Beta") {
-        const videoData = {
-          videoInfo: this.props.currentUrl,
-          isZawgyi: this.props.isZawgyi,
-        };
-
-        dynamicVideoPlayer = <VideoPlayer {...videoData} />;
-      } else {
-        dynamicVideoPlayer = (
-          <iframe
-            title={this.props.currentVideoServerName}
-            src={this.props.serverNameUrlMap[this.props.currentVideoServerName]}
-            frameBorder="0"
-            marginWidth="0"
-            marginHeight="0"
-            scrolling="no"
-            width="100%"
-            height="100%"
-            allowFullScreen
-          ></iframe>
-        );
-      }
-    }
-
-    if (this.props.episodes && this.props.name) {
+  if (props.animeInfo && episodeInfo) {
+    if (props.animeInfo.anime_id === episodeInfo.anime_id) {
       episodesList = (
         <EpisodesList
-          isZawgyi={this.props.isZawgyi}
-          number={this.props.episodes}
-          animeName={this.props.name}
+          isZawgyi={props.isZawgyi}
+          number={props.animeInfo.number_of_episodes}
+          animeId={props.animeInfo.anime_id}
+          episodes={props.animeInfo.episodes}
         />
       );
     }
-
-    dynamicContent = (
-      <Fragment>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>{`${pathNames[2]} ${pathNames[3]} | MYAN-nime`}</title>
-        </Helmet>
-        <Breadcrumb name={pathNames[2]} episode={pathNames[3]} />
-        <Grid container item>
-          <FormControl style={{ width: "100px", margin: "5px 0" }}>
-            {serverSelector}
-          </FormControl>
-        </Grid>
-        <Grid item>
-          <br />
-          <Typography variant="subtitle2" className={classes.Gomen}>
-            {this.props.isZawgyi ? toZawgyi(this.ADS_INFO) : this.ADS_INFO}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <br />
-          <Typography variant="subtitle2" className={classes.Gomen}>
-            {this.props.isZawgyi
-              ? toZawgyi(this.ERROR_REPORT)
-              : this.ERROR_REPORT}
-            <Link
-              href="https://m.me/myanNime"
-              target="_blank"
-              rel="noopener noreferer"
-              color="primary"
-            >
-              Messenger
-            </Link>
-          </Typography>
-        </Grid>
-        <Grid item>
-          <br />
-          <Typography variant="subtitle2" className={classes.Gomen}>
-            {this.props.isZawgyi
-              ? toZawgyi(this.LIKE_SOCIAL)
-              : this.LIKE_SOCIAL}
-          </Typography>
-          <iframe
-            src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FmyanNime&width=78&layout=button&action=like&size=large&share=false&height=30&appId"
-            width="78"
-            height="30"
-            title="Facebook Like"
-            scrolling="no"
-            frameBorder="0"
-            allow="encrypted-media"
-            className={classes.Fb}
-          ></iframe>
-        </Grid>
-        <Grid item className={classes.IframeContainer}>
-          {dynamicVideoPlayer}
-        </Grid>
-        <Grid item>
-          {this.state.showComments ? (
-            <Paper className={classes.Paper}>{disqusTag}</Paper>
-          ) : (
-            <Button
-              variant="contained"
-              color="secondary"
-              size="large"
-              onClick={() => {
-                this.setState({ showComments: true });
-              }}
-            >
-              {this.props.isZawgyi
-                ? toZawgyi(this.SHOW_COMMENTS)
-                : this.SHOW_COMMENTS}
-            </Button>
-          )}
-          <Paper className={classes.Paper}>{episodesList}</Paper>
-        </Grid>
-      </Fragment>
-    );
-
-    if (this.props.loading) {
-      dynamicContent = <Loader />;
-    }
-
-    if (this.props.error) {
-      dynamicContent = <ErrorMessage isZawgyi={this.props.isZawgyi} />;
-    }
-
-    return (
-      <SnackbarProvider maxSnack={3}>
-        <Grid container>
-          <Grid item xs={1} sm={2}></Grid>
-          <Grid item xs={10} sm={8}>
-            {dynamicContent}
-          </Grid>
-          <Grid item xs={1} sm={2}></Grid>
-        </Grid>
-      </SnackbarProvider>
-    );
   }
-}
+
+  dynamicContent = (
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{`${props.animeInfo ? props.animeInfo.title : ""} Episode: ${
+          episodeInfo ? episodeInfo.episode_number : ""
+        } | MYAN-nime`}</title>
+      </Helmet>
+      <Breadcrumb
+        name={props.animeInfo ? props.animeInfo.title : ""}
+        episode={"Episode: " + (episodeInfo && episodeInfo.episode_number)}
+        anime_id={props.animeInfo ? props.animeInfo.anime_id : null}
+      />
+      <Grid container item>
+        <FormControl style={{ width: "100px", margin: "5px 0" }}>
+          {serverSelector}
+        </FormControl>
+      </Grid>
+      <Grid item style={{ marginTop: "8px" }}>
+        <Typography>
+          {props.isZawgyi ? toZawgyi(THANKS_MESSAGE) : THANKS_MESSAGE}
+        </Typography>
+      </Grid>
+      <Grid item className={classes.IframeContainer}>
+        {dynamicVideoPlayer}
+      </Grid>
+      <Grid item>
+        {showComments ? (
+          <Paper className={classes.Paper}>{disqusTag}</Paper>
+        ) : (
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={() => {
+              setShowComments(true);
+            }}
+          >
+            {props.isZawgyi ? toZawgyi(SHOW_COMMENTS) : SHOW_COMMENTS}
+          </Button>
+        )}
+        <Paper className={classes.Paper}>{episodesList}</Paper>
+      </Grid>
+    </Fragment>
+  );
+
+  if (props.loading) {
+    dynamicContent = <Loader />;
+  }
+
+  if (props.error) {
+    dynamicContent = <ErrorMessage isZawgyi={props.isZawgyi} />;
+  }
+
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <Grid container>
+        <Grid item xs={1} sm={2}></Grid>
+        <Grid item xs={10} sm={8}>
+          {dynamicContent}
+        </Grid>
+        <Grid item xs={1} sm={2}></Grid>
+      </Grid>
+    </SnackbarProvider>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
-    currentEpisode: state.video.currentEpisode,
-    currentUrl: state.video.currentUrl,
-    videoServers: state.video.videoServers,
-    currentVideoServerName: state.video.currentVideoServerName,
-    streamUrls: state.video.streamUrls,
-    serverNameUrlMap: state.video.serverNameUrlMap,
+    episodeInfo: state.video.episodeInfo,
+    animeInfo: state.info.animeInfo,
     loading: state.video.loading,
     error: state.video.error,
-    name: state.info.name,
-    episodes: state.info.episodes,
     isZawgyi: state.mmfont.isZawgyi,
   };
 };
@@ -274,9 +213,6 @@ const mapDispatchToProps = (dispatch) => {
   return {
     handleFetchVideoData: (pathName, redirectFunc) => {
       dispatch(actions.fetchVideoData(pathName, redirectFunc));
-    },
-    handleChangeStreamUrl: (newName, newUrl) => {
-      dispatch(actions.changeStreamUrl(newName, newUrl));
     },
   };
 };
