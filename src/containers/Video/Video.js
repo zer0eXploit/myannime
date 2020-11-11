d879d5ac4f0a34ff5eba5d2d135efa0ef0fc366a
Yan Waipann <yanwaipann@gmail.com>
2020-11-11 14:31:21 +0630

    Make the player responsive

--- src/containers/Video/Video.js
+++ src/containers/Video/Video.js
@@ -105,17 +105,24 @@ const Video = (props) => {
 
   if (currentUrl) {
     dynamicVideoPlayer = (
-      <iframe
-        title={currentUrl}
-        src={currentUrl}
-        frameBorder="0"
-        marginWidth="0"
-        marginHeight="0"
-        scrolling="no"
-        width="100%"
-        height="100%"
-        allowFullScreen
-      ></iframe>
+      <Fragment>
+        <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+          <iframe
+            title={currentUrl}
+            src={currentUrl}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      </Fragment>
     );
   }
 
