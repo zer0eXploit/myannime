de8737fb626803bc522fbeeb7b10375d7d56ff41
Yan Waipann <yanwaipann@gmail.com>
2021-01-27 16:43:24 +0630

    Add Request New Activation Email Component

---
+++ src/containers/Auth/Register/ActivationEmail/ActivationEmail.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Grid, Paper, Button } from "@material-ui/core";
+import { withSnackbar } from "notistack";
+
+import axios from "../../../../util/axiosMyannime";
+
+import styles from "./ActivationEmail.module.css";
+
+const ActivationEmail = (props) => {
+  const [username, setUsername] = useState("");
+  const [usernameValid, setUsernameValid] = useState(false);
+
+  const usernameIsValid = (username) => {
+    return username.length >= 3;
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const formData = {
+      username: username,
+    };
+
+    try {
+      setUsernameValid(false);
+      const response = await axios.post(
+        "/user/resend_activation_email",
+        formData,
+      );
+      const data = response.data;
+      props.enqueueSnackbar(data.message, { variant: "info" });
+      setUsername("");
+    } catch (e) {
+      console.log(e.response);
+      props.enqueueSnackbar(e.response.data.message, { variant: "error" });
+      setUsername("");
+      setUsernameValid(false);
+    }
+  };
+
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+    setUsernameValid(usernameIsValid(e.target.value));
+  };
+
+  return (
+    <Grid container justify="center">
+      <Paper className={styles.Paper}>
+        <div className={styles.FormContainer}>
+          <h3 className={styles.Heading}>Request for a new activation email</h3>
+          <p className={styles.Info}>Please enter your username below.</p>
+          <form onSubmit={submitHandler}>
+            <label className={styles.Label}>Username</label>
+            <input
+              type="text"
+              className={styles.InputElement}
+              value={username}
+              onChange={usernameChangeHandler}
+            ></input>
+            <Button
+              className={styles.SubmitButton}
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={!usernameValid}
+            >
+              Request
+            </Button>
+          </form>
+        </div>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default withSnackbar(ActivationEmail);
