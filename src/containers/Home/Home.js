f3aa7e5e4906e6f106c7c2dc6aab219483daabe6
Yan Waipann <yanwaipann@gmail.com>
2021-09-29 21:41:42 +0630

    Refactor: Update to functional component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,115 +1,107 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Grid } from "@material-ui/core";
+import React from "react";
+
+import { useEffect, useCallback } from "react";
 import { Helmet } from "react-helmet";
+import { Grid } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+
 import Card from "../../components/Card/Card";
+import Radio from "../../components/Radio/Radio";
 import Loader from "../../components/Loader/Loader";
 import Pagnation from "../../components/Pagination/Pagination";
-import Radio from "../../components/Radio/Radio";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
 import * as actions from "../../store/actions/index";
 
-class Home extends Component {
-  componentDidMount() {
-    if (this.props.animeData) {
-      return;
-    }
-    this.props.fetchAnimeData();
-  }
+function MyanNimeHome(props) {
+  const dispatch = useDispatch();
 
-  render() {
-    let dynamicContent = null;
-    let pagination = null;
-    let sortMethod = null;
+  const error = useSelector((state) => state.home.error);
+  const loading = useSelector((state) => state.home.loading);
+  const isZawgyi = useSelector((state) => state.home.isZawgyi);
+  const animeData = useSelector((state) => state.home.animeData);
+  const sortMethod = useSelector((state) => state.home.sortMethod);
 
-    if (this.props.loading) {
-      dynamicContent = <Loader />;
-    }
-
-    if (!this.props.loading && this.props.animeData) {
-      dynamicContent = this.props.animeData.animes.map((anime) => {
-        return (
-          <Grid
-            item
-            container
-            justify="center"
-            key={anime.anime_id}
-            xs={6}
-            sm={4}
-            md={3}
-            lg={2}
-          >
-            <Card {...anime} isZawgyi={this.props.isZawgyi} />
-          </Grid>
-        );
-      });
-
-      pagination = (
-        <Grid item container justify={"center"}>
-          <Pagnation
-            totalPages={this.props.animeData.total_pages}
-            currentPage={this.props.animeData.current_page}
-            handlePaginate={this.props.fetchAnimeData}
-            sortMethod={this.props.sortMethod}
-          />
-        </Grid>
-      );
-
-      sortMethod = (
-        <Grid style={{ marginTop: "2%" }} item container justify="center">
-          <Radio
-            currentPage={this.props.animeData.current_page}
-            handlePaginate={this.props.fetchAnimeData}
-            sortMethod={this.props.sortMethod}
-          />
-        </Grid>
-      );
-    }
-
-    if (this.props.error) {
-      dynamicContent = <ErrorMessage isZawgyi={this.props.isZawgyi} />;
-    }
-
-    return (
-      <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Home | MYAN-nime</title>
-        </Helmet>
-        <Grid item container xs={12} sm={10} style={{ maxWidth: "1500px" }}>
-          <Grid
-            container
-            spacing={1}
-            justify="flex-start"
-            style={{ margin: 0 }}
-          >
-            {dynamicContent}
-          </Grid>
-          {sortMethod}
-          {pagination}
-        </Grid>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    animeData: state.home.animeData,
-    loading: state.home.loading,
-    sortMethod: state.home.sortMethod,
-    error: state.home.error,
-    isZawgyi: state.mmfont.isZawgyi,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchAnimeData: (page, sortBy) => {
+  const handlePaginate = useCallback(
+    (page, sortBy) => {
       dispatch(actions.fetchAnimeData(page, sortBy));
     },
-  };
-};
+    [dispatch],
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+  useEffect(() => {
+    if (animeData) return;
+
+    handlePaginate();
+  }, [dispatch, animeData, handlePaginate]);
+
+  let dynamicContent = null;
+  let pagination = null;
+  let sortingMethod = null;
+
+  if (loading) {
+    dynamicContent = <Loader />;
+  }
+
+  if (!loading && animeData) {
+    dynamicContent = animeData.animes.map((anime) => {
+      return (
+        <Grid
+          item
+          container
+          justify="center"
+          key={anime.anime_id}
+          xs={6}
+          sm={4}
+          md={3}
+          lg={2}
+        >
+          <Card {...anime} isZawgyi={isZawgyi} />
+        </Grid>
+      );
+    });
+
+    pagination = (
+      <Grid item container justify={"center"}>
+        <Pagnation
+          totalPages={animeData.total_pages}
+          currentPage={animeData.current_page}
+          handlePaginate={handlePaginate}
+          sortMethod={sortMethod}
+        />
+      </Grid>
+    );
+
+    sortingMethod = (
+      <Grid style={{ marginTop: "2%" }} item container justify="center">
+        <Radio
+          currentPage={animeData.current_page}
+          handlePaginate={handlePaginate}
+          sortMethod={sortMethod}
+        />
+      </Grid>
+    );
+  }
+
+  if (error) {
+    dynamicContent = <ErrorMessage isZawgyi={isZawgyi} />;
+  }
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home | MYAN-nime</title>
+      </Helmet>
+      <Grid item container xs={12} sm={10} style={{ maxWidth: "1500px" }}>
+        <Grid container spacing={1} justify="flex-start" style={{ margin: 0 }}>
+          {dynamicContent}
+        </Grid>
+        {sortingMethod}
+        {pagination}
+      </Grid>
+    </div>
+  );
+}
+
+export default MyanNimeHome;
