bad11cc31b667473f55f5ae5e5dba7011a9b8c45
Yan Waipann <yanwaipann@gmail.com>
2021-09-29 22:00:47 +0630

    Outsource genreInfo themes

---
+++ src/config/genreInfoTheme.js
@@ -0,0 +1,12 @@
+const styler = (theme) => ({
+  genreIntro: {
+    padding: "2%",
+    borderRadius: "0",
+    marginBottom: "3%",
+  },
+  title: {
+    marginBottom: "15px",
+  },
+});
+
+export default styler;
