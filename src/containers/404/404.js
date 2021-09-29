e7c516a6920ff3b11993945a736b2e29111ffdfa
Yan Waipann <yanwaipann@gmail.com>
2021-09-29 22:10:42 +0630

    Refactor: Update to functional component

--- src/containers/404/404.js
+++ src/containers/404/404.js
@@ -1,55 +1,43 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import Helmet from "react-helmet";
+
 import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { useSelector } from "react-redux";
+
 import toZawgyi from "../../util/convertToZg";
+
 import styles from "./FourOFour.module.css";
 
-class FourOFour extends Component {
-  SORRY_TEXT = "တောင်းပန်ပါသည်။";
-  NOT_FOUND_TEXT = "သင်ရှာဖွေနေသောအရာ ကျွန်ုပ်တို့ဆီမှာ မရှိပါ။";
-  CHECK_LINK_TEXT = "Link URL ကို သေချာ ပြန်စစ်ကြည့်စေလိုပါသည်။";
-  OR_TEXT = "သို့မဟုတ်";
-  BACK_TO_HOME_TEXT = "ပင်မစာမျက်နှာသို့";
+function NotFound(props) {
+  const isZawgyi = useSelector((state) => state.mmfont.isZawgyi);
 
-  render() {
-    return (
-      <div className={styles.ErrorPage}>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>404 | MYAN-nime</title>
-        </Helmet>
-        <h3>
-          {this.props.isZawgyi ? toZawgyi(this.SORRY_TEXT) : this.SORRY_TEXT}
-          <br />
-          {this.props.isZawgyi
-            ? toZawgyi(this.NOT_FOUND_TEXT)
-            : this.NOT_FOUND_TEXT}
+  const SORRY_TEXT = "တောင်းပန်ပါသည်။";
+  const NOT_FOUND_TEXT = "သင်ရှာဖွေနေသောအရာ ကျွန်ုပ်တို့ဆီမှာ မရှိပါ။";
+  const CHECK_LINK_TEXT = "Link URL ကို သေချာ ပြန်စစ်ကြည့်စေလိုပါသည်။";
+  const OR_TEXT = "သို့မဟုတ်";
+  const BACK_TO_HOME_TEXT = "ပင်မစာမျက်နှာသို့";
 
-          <br />
-          {this.props.isZawgyi
-            ? toZawgyi(this.CHECK_LINK_TEXT)
-            : this.CHECK_LINK_TEXT}
-
-          <br />
-          {this.props.isZawgyi ? toZawgyi(this.OR_TEXT) : this.OR_TEXT}
-
-          <br />
-          <Link to="/">
-            {this.props.isZawgyi
-              ? toZawgyi(this.BACK_TO_HOME_TEXT)
-              : this.BACK_TO_HOME_TEXT}
-          </Link>
-        </h3>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.ErrorPage}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>404 | MYAN-nime</title>
+      </Helmet>
+      <h3>
+        {isZawgyi ? toZawgyi(SORRY_TEXT) : SORRY_TEXT}
+        <br />
+        {isZawgyi ? toZawgyi(NOT_FOUND_TEXT) : NOT_FOUND_TEXT}
+        <br />
+        {isZawgyi ? toZawgyi(CHECK_LINK_TEXT) : CHECK_LINK_TEXT}
+        <br />
+        {isZawgyi ? toZawgyi(OR_TEXT) : OR_TEXT}
+        <br />
+        <Link to="/">
+          {isZawgyi ? toZawgyi(BACK_TO_HOME_TEXT) : BACK_TO_HOME_TEXT}
+        </Link>
+      </h3>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isZawgyi: state.mmfont.isZawgyi,
-  };
-};
-
-export default connect(mapStateToProps)(FourOFour);
+export default NotFound;
