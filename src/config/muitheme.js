b34b66e57d6d4293a85cd2971dd8fb7949183522
Yan Waipann <yanwaipann@gmail.com>
2021-09-29 20:09:04 +0630

    Add MaterialUI theme config

---
+++ src/config/muitheme.js
@@ -0,0 +1,15 @@
+const themeInfo = {
+  palette: {
+    primary: {
+      main: "#f44336",
+    },
+    secondary: {
+      main: "#1976d2",
+    },
+  },
+  typography: {
+    fontFamily: ['"Myanmar Sans Pro"', '"Open Sans"', "sans-serif"].join(","),
+  },
+};
+
+export default themeInfo;
