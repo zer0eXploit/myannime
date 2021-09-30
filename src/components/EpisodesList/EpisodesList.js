617852b98c243a6da0cd06a6680a79444e0dbbdf
Yan Waipann <yanwaipann@gmail.com>
2021-09-30 19:38:16 +0630

    Use translations from i18n for UI text

--- src/components/EpisodesList/EpisodesList.js
+++ src/components/EpisodesList/EpisodesList.js
@@ -1,141 +1,40 @@
-import React, { Fragment, Component } from "react";
-import { ListItem, ListItemText, Typography } from "@material-ui/core";
-// import { Waypoint } from "react-waypoint";
+import React from "react";
+
+import { Fragment } from "react";
+import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
-// import Loader from "../Loader/Loader";
-import toZawgyi from "../../util/convertToZg";
+import { ListItem, ListItemText, Typography } from "@material-ui/core";
 
-class EpisodesList extends Component {
-  // state = {
-  //   episodesList: [],
-  //   moreToLoad: true,
-  //   loading: true,
-  //   lastEpisodeNumber: 20,
-  // };
+function EpisodesList(props) {
+  const { t } = useTranslation();
 
-  // handleWayPointEnter = () => {
-  //   if (this.state.moreToLoad) {
-  //     this.setState({ ...this.state, loading: true });
+  let episodesList = null;
 
-  //     const episodes = this.appendToList(this.state.lastEpisodeNumber + 1);
-  //     this.setState({
-  //       ...this.state,
-  //       episodesList: [...this.state.episodesList, ...episodes],
-  //       moreToLoad: this.props.number > this.state.episodesList.length + 20,
-  //       //+20 because state (async) is not updated yet to account for newly appended episodes
-  //       lastEpisodeNumber: this.state.episodesList.length + episodes.length,
-  //       //same here
-  //       loading: false,
-  //     });
-  //   }
-  // };
-
-  // appendToList = (start) => {
-  //   let episodes = [];
-  //   let end = start + 20;
-  //   if (end > this.props.number) {
-  //     end = this.props.number + 1;
-  //   }
-  //   for (let i = start; i < end; i++) {
-  //     if (i === end - 1) {
-  //       episodes.push(
-  //         <Waypoint
-  //           onEnter={this.handleWayPointEnter}
-  //           key={"Episode" + i + new Date().getTime()}
-  //         >
-  //           <ListItem
-  //             button
-  //             component={RouterLink}
-  //             to={"/Anime/" + this.props.animeId + "/" + this.props.animeId}
-  //           >
-  //             <ListItemText primary={"Episode " + i} />
-  //           </ListItem>
-  //         </Waypoint>
-  //       );
-  //     } else {
-  //       episodes.push(
-  //         <ListItem
-  //           button
-  //           key={"Episode" + i + new Date().getTime()}
-  //           component={RouterLink}
-  //           to={"/Anime/" + this.props.animeId + "/" + this.props.animeId}
-  //         >
-  //           <ListItemText primary={"Episode " + i} />
-  //         </ListItem>
-  //       );
-  //     }
-  //   }
-  //   return episodes;
-  // };
-
-  // componentDidMount() {
-  //   console.log(this.props);
-  //   const episodes = this.appendToList(1);
-  //   this.setState({
-  //     ...this.state,
-  //     episodesList: [...this.state.episodesList, ...episodes],
-  //     moreToLoad: this.props.number > 20,
-  //     lastEpisodeNumber: episodes.length,
-  //     loading: false,
-  //   });
-  // }
-
-  // render() {
-  //   let episodesList = null;
-
-  //   if (this.state.episodesList.length > 0) {
-  //     episodesList = this.state.episodesList.map((episode) => {
-  //       return episode;
-  //     });
-  //   }
-
-  //   return (
-  //     <Fragment>
-  //       <ListItem>
-  //         <Typography variant="h5">
-  //           {this.props.isZawgyi
-  //             ? toZawgyi("ဇာတ်လမ်း အပိုင်းများ")
-  //             : "ဇာတ်လမ်း အပိုင်းများ"}
-  //         </Typography>
-  //       </ListItem>
-  //       {episodesList}
-  //       {this.state.loading && <Loader />}
-  //     </Fragment>
-  //   );
-  // }
-
-  render() {
-    let episodesList = null;
-
-    if (this.props.episodes.length > 0) {
-      episodesList = this.props.episodes.map((episode) => {
-        return (
-          <ListItem
-            button
-            key={episode.episode_id}
-            component={RouterLink}
-            to={"/Anime/" + this.props.animeId + "/" + episode.episode_id}
-          >
-            <ListItemText primary={"Episode " + episode.episode_number} />
-          </ListItem>
-        );
-      });
-    }
-
-    return (
-      <Fragment>
-        <ListItem>
-          <Typography variant="h5">
-            {this.props.isZawgyi
-              ? toZawgyi("ဇာတ်လမ်း အပိုင်းများ")
-              : "ဇာတ်လမ်း အပိုင်းများ"}
-          </Typography>
+  if (props.episodes.length > 0) {
+    episodesList = props.episodes.map((episode) => {
+      return (
+        <ListItem
+          button
+          key={episode.episode_id}
+          component={RouterLink}
+          to={"/Anime/" + props.animeId + "/" + episode.episode_id}
+        >
+          <ListItemText
+            primary={`${t("episodesList.episode")} ${episode.episode_number}`}
+          />
         </ListItem>
-        {episodesList}
-        {/* {this.state.loading && <Loader />} */}
-      </Fragment>
-    );
+      );
+    });
   }
+
+  return (
+    <Fragment>
+      <ListItem>
+        <Typography variant="h5">{t("episodesList.episodes")}</Typography>
+      </ListItem>
+      {episodesList}
+    </Fragment>
+  );
 }
 
 export default EpisodesList;
